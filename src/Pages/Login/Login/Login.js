6898b05bd9a1ba1e6c57c751a2fe32dc3342dfff
Ahmad Musa <amaahmadmusa@gmail.com>
2022-10-25 23:06:26 -0700

    login form is added

---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,79 @@
+
+import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
+import { GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { FaGoogle, FaGithub} from "react-icons/fa";
+
+
+const Login = () => {
+
+    const {signIn, providerLogin} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            navigate('/')
+        })
+        .catch(error => console.error(error))
+    }
+
+    const handleGoogleSign = () => {
+        providerLogin(googleProvider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.error(error))
+    }
+
+    const handleGithubSign = () => {
+        providerLogin(githubProvider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.error(error))
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control name="email" type="email" placeholder="Enter email" required />
+
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control name="password" type="password" placeholder="Password" required />
+        </Form.Group>
+
+        <Form.Text >
+            <small>New to this website? Please <Link to='/register'>Register</Link> </small>
+        </Form.Text>
+        <br></br>
+        <Button className="my-2" variant="primary" type="submit">
+            Login
+        </Button>
+          <br></br>
+        <Button onClick={handleGoogleSign} variant="outline-primary"> <FaGoogle></FaGoogle> Google SignIn</Button>
+        <Button onClick={handleGithubSign} variant="outline-dark"> <FaGithub></FaGithub> Github SignIn</Button>
+    </Form>
+    );
+};
+
+export default Login;
